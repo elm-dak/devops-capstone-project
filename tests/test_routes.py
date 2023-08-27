43d95e0043d3e8190971b18500741206ca35fa27
DAKOUKY ELMESTAPHA <DAKOUKY_ELMESTAPHA@emsi-edu.ma>
2023-08-27 16:18:24 -0400

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -14,6 +14,8 @@ from service.models import db, Account, init_db
 from service.routes import app
 from service import talisman
 
+AssertionError: None != '*'
+
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
@@ -184,7 +186,6 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
-
     def test_cors_security(self):
         """It should return a CORS header"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
