81a2696a3ac0de8e7dc231656f95b94f9e318b16
Dakouky ELMESTAPHA <DAKOUKY_ELMESTAPHA@emsi-edu.ma>
2023-08-27 14:42:03 -0400

    completed ci build

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -30,8 +30,8 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.get(
             f"{BASE_URL}/{account.id}", content_type="application/json"
-            )
-        self.assertEqual(resp.status_code, status.HTTP_202_OK)
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)  # Corrected status code
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
     @classmethod
